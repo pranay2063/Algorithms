5b35237e93099ef2ef8f85bc4daf4c6c8b8e5c1a
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-03-24 07:15:17 +0530

    Breadth First Search

--- GRAPHS/BFS.cpp
+++ GRAPHS/BFS.cpp
@@ -20,26 +20,19 @@ void BFS(int start, int visited[])
 
 	while(!Q.empty())
 	{
-
 		int u = Q.front();
 		printf("%d ", u);
-
 		for(int i = 0; i != adj[u].size(); ++i)
 		{
-
 			int v = adj[u][i];
 			if(!visited[v])
 			{
-
 				visited[v] = 1;
 				Q.push(v);
-
 			}
-
 		}
 
 		Q.pop();
-
 	}
 
 }
@@ -48,20 +41,16 @@ int main()
 {
 
 	int a,b,n,m,i;
-
 	scanf("%d%d",&n,&m); // n is the number of vertices and m is the number of edges
 
 	for(i = 0; i < m; ++i)
 	{
-
 		scanf("%d%d",&a,&b);
 		adj[a].push_back(b);
 		adj[b].push_back(a);
-
 	}
 
 	int visited[MAX] = {0};
-
 	for(i = 1; i <= n; ++i)
 		if(!visited[i])
 			BFS(i,visited);
