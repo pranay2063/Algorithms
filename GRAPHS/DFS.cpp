c682b6917b8ec025272657f84dc0c0a3da43b2c1
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-03-24 07:18:04 +0530

    Depth First Search of an undirected graph

--- GRAPHS/DFS.cpp
+++ GRAPHS/DFS.cpp
@@ -10,14 +10,11 @@
 #include<iostream>
 
 using namespace std;
-
 const int MAX = 100001;
-
 void dfs(vector<int> adj[],int visited[],int parent[],int start)
 {
 
 	// non-recursive version
-
 	stack<int> a;
 	a.push(start);
 	parent[start] = -1; // arbitrary value
@@ -25,25 +22,19 @@ void dfs(vector<int> adj[],int visited[],int parent[],int start)
 
 	while(!a.empty())
 	{
-
 		int u = a.top();
 		cout<<u<<" ";
 		a.pop();
 		for(int i=0;i!=adj[u].size();i++)
 		{
-
 			int v = adj[u][i];
 			if(!visited[v])	
 			{
-
 				visited[v] = 1;
 				a.push(v);
 				parent[v] = u;
-
 			}
-
 		}
-
 	}
 
 }
@@ -59,12 +50,9 @@ void dfs2(vector<int> adj[],int visited[],int parent[],int u)
 		int v = adj[u][i];
 		if(!visited[v])
 		{
-
 			parent[v] = u;
 			dfs2(adj,visited,parent,v);
-
 		}
-
 	}
 
 }
@@ -73,34 +61,24 @@ int main()
 {
 
 	int n,m,a,b,i;
-
 	cin>>n>>m;
-
 	vector<int> adj[n+1];
-
 	for(i=0;i<m;i++)
 	{
-
 		cin>>a>>b;
 		adj[a].push_back(b);
 		adj[b].push_back(a);  // Graph is assumed to be undirected
-
 	}
 
 	int visited[MAX] = {0};
 	int parent[n+1];
-	
 	for(i=1;i<=n;i++)
 	{
-
 		if(!visited[i])
 		{
-
 			parent[i] = -1;
 			dfs(adj,visited,parent,i);
-
 		}
-
 	} 
 
 	return 0;
