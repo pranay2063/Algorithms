79f8628c0ecbb5fffcd9675a02f872bcc010d081
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-03-25 23:31:51 +0530

    Maxheap class

--- SORTING/MAXHEAP CLASS.cpp
+++ SORTING/MAXHEAP CLASS.cpp
@@ -23,9 +23,7 @@ class max_heap{
              void restore_up(int );
              void top_to_down();
              void bottom_up();
-
              void heap_sort();
-
              void print();
 
 };
@@ -35,12 +33,11 @@ max_heap :: max_heap(int b[],int len)
 
     //max_heap::
     size=len;
-
     a=(int *)malloc((size+1)*sizeof(int));
 
     for(int i=0;i<=size;i++)
-    //max_heap::
-    a[i]=b[i];
+      //max_heap::
+      a[i]=b[i];
 
 }
 
@@ -49,7 +46,6 @@ void max_heap :: print()
 
     for(int i=1;i<size;i++)
     cout<<a[i]<<endl;
-
     cout<<endl;
 
 }
@@ -69,11 +65,9 @@ void max_heap :: restore_up(int size)
 
      while(a[parent]<a[child] && parent>=1)
      {
-
          swap(a[parent],a[child]);
          child=child/2;
          parent=parent/2;
-
      }
 
 }
@@ -93,30 +87,19 @@ void max_heap :: restore_down(int root)
 
      while(left<size && right<size)
      {
-
-         if(a[root]>=a[left] && a[root]>=a[right])
-         {
-
-             break;
-
-         }
+         if(a[root]>=a[left] && a[root]>=a[right]) break;
          else if(a[left]>=a[right])
               {
-
                    swap(a[left],a[root]);
                    root=left;
-
               }
               else {
-
                         swap(a[right],a[root]);
                         root=right;
-
                    }
 
          left=2*root;
          right=left+1;
-
      }
 
      if(left<size && a[left]>a[root])
@@ -128,20 +111,16 @@ void max_heap :: heap_sort()
 {
 
     int temp=size;
-
     bottom_up();
 
     for(int i=2;i<temp;i++)
     {
-
         swap(a[1],a[size-1]);
         --size;
         restore_down(1);
-
     }
 
     size=temp;
-
 }
 
 int main()
@@ -150,15 +129,11 @@ int main()
     int a[]={INT_MAX,34,56,78,89,90,134,56};
 
     max_heap obj(a,8);
-
+   
     obj.print();
-
     obj.bottom_up();
-
     obj.print();
-
     obj.heap_sort();
-
     obj.print();
 
     return 0;
