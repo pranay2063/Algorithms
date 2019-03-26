4cac64bdd562097d8f8fd294ced36a89150e43f6
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-03-26 14:47:08 +0530

    Added priority queue STL implementation

---
+++ PriorityQueue/PriorityQueueSTL.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct data{
+	int a;
+	char c;
+	data(int a, char c): a(a), c(c){ }
+	bool operator<(const data& rhs) const {
+		return a < rhs.a;
+	}
+};
+
+int main() {
+	
+	priority_queue<data> pq;
+	//data x = {42,'a'};
+	pq.push(data(42,'a'));
+	//x = {49,'b'};
+	pq.push(data(49,'c'));
+	//x = {53,'c'};
+	pq.push(data(53,'b'));
+	
+	priority_queue<data> tmp = pq;
+	while(!tmp.empty()){
+		data pq_top = tmp.top();
+		cout<<pq_top.a<<" "<<pq_top.c<<endl;
+		tmp.pop();
+	}
+	
+	//53 b
+	//49 c
+	//42 a
+	
+	return 0;
+}
