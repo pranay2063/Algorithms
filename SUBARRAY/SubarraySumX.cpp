659f20c4485c659850685aa9021ed0cb77122e7d
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-03-27 13:45:04 +0530

    Find sub-array from the given array such that sum of all the elements of that sub-array is equal to X

--- SUBARRAY/SubarraySumX.cpp
+++ SUBARRAY/SubarraySumX.cpp
@@ -5,79 +5,51 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
 
 const int MAX  = 1000005;
-
 int a[MAX];
 
 int main()
 {
     int t,i;
     ll n,x;
-    
     scanf("%d",&t);
     
     while(t--)
     {
-    	
-    	  scanf("%lld%lld",&n,&x);
-    	
     	  //n is the number of elements in the array
     	  //x is the given value
+    	  scanf("%lld%lld",&n,&x);
     	
-    	  for(i=0;i<n;i++)
-    	  {
-    		
-    		    scanf("%lld",&a[i]);
-    		
-    	  }
+    	  for(i=0;i<n;i++)  scanf("%lld",&a[i]);
     	
     	  bool ans=false;
-    	
     	  ll sum=0,start=-1;
-    	
-    	
-    	  for(i=0;i<n;i++)
-    	  {
+ 
+    	  for(i=0;i<n;i++){
     		
     		    sum+=a[i];
-    		
     		    if(sum>x)
     		    {
-    			
-    			      while(sum>x)
-    			      {
-    				
-    				        sum-=a[++start];
-    				
-    				        //remove elements from the beginning
-    				
-    			      }
+    			      while(sum>x)  sum-=a[++start];    //remove elements from the beginning
     			
     			      if(sum==x) 
     			      {
-    				
     				        ans=true;
     				        break;
-    				
     			      }
-    			
     		    }
     		    else if(sum==x)
     			       {
-    			 	
     			 	          ans=true;
     			 	          break;
-    			 	
     			       }
     			 
     		
     	  }
     	
     	  if(ans) cout<<"YES"<<"\n";
-    	
     	  else cout<<"NO"<<"\n";
     	
     }
