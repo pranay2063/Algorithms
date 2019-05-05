490688ce02234c9b2380aed584509c1529f8fd68
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-05-05 07:21:29 +0530

    Lowest Common Ancestor

--- OJ/SPOJ/LCA(SPOJ).cpp
+++ OJ/SPOJ/LCA(SPOJ).cpp
@@ -13,7 +13,6 @@
 */  
 
 #include<bits/stdc++.h>
-
 using namespace std;
 
 vector<int> node[1001]; // array of vectors
@@ -22,133 +21,90 @@ int main()
 {
 
     int t,i,m,n,num,cases;
-
     scanf("%d",&t);
-
+    
     cases=t;
-
     while(t--)
     {
 
         scanf("%d",&n);
-
         vector<int> node[n+1];
         vector<int> parent(n+1,0);
 
         for(i=1;i<=n;i++)
         {
-
              scanf("%d",&m);
-
              // m is the number of children of i
-
              while(m--)
              {
-
                  scanf("%d",&num);
                  node[i].push_back(num);
-
                  // i is the parent of all the nodes being scanned
-
                  parent[num]=i;
-
              }
-
              // Here tree is being maintained as array of vectors with ith row containing children of i
-
         }
 
         int root;
-
         //find root
-
         //Root is the node which does not have any parent
 
         for(i=1;i<=n;i++)
         {
-
             if(parent[i]==0)
             {
-
                 root=i;
-
                 break;
-
             }
-
         }
 
         //printf("%d\n",root);
-
+        
         //BFS
-
         int u,v;
-
         vector<bool> visited(n+1,false);
         vector<int> depth(n+1,0);
-
+       
         //BFS traversal assigns depth of nodes according to height with root at highest depth and nodes at
         //same level at same depth
 
         queue<int> BFS;
-
         BFS.push(root);
-
         visited[root]=true;
 
         while(!BFS.empty())
         {
-
             u=BFS.front();
-
             for(i=0;i!=node[u].size();i++)
             {
-
                 v=node[u][i];
-
                 if(visited[v]==false)
                 {
-
                     visited[v]=true;
-
                     depth[v]=depth[parent[v]]+1;
-
                     BFS.push(v);
-
                 }
-
             }
-
             BFS.pop();
-
         }
 
         //Queries
         int Queries;
-
         scanf("%d",&Queries);
-
         printf("Case %d:\n",cases-t);
 
         while(Queries--)
         {
-
             scanf("%d%d",&u,&v);
-
             //Lowest Common Ancestor(LCA)
-
             if(depth[u]>depth[v])
-            while(depth[u]>depth[v]) u=parent[u];
-
+               while(depth[u]>depth[v]) u=parent[u];
             else if(depth[u]<depth[v])
                  while(depth[u]<depth[v]) v=parent[v];
 
-
             while(u!=v)
             u=parent[u],v=parent[v];
-
             printf("%d\n",u);
-
         }
 
     }
