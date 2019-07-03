c0fbfef7ecd1f771bd3f19971fce4c4020738ce3
Pranay Ranjan <pranay2063@users.noreply.github.com>
2019-07-03 15:21:33 +0530

    Longest Common Prefix (Easy)

---
+++ OJ/STRINGS/LongestCommonPrefix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int minlen = INT_MAX;
+        for(int i = 0; i < strs.size(); ++i)
+            minlen = (strs[i].length() < minlen)?strs[i].length():minlen;
+        bool flag = true;
+        string ans;
+        if(minlen == INT_MAX) return ans;
+        for(int i = 0; i < minlen; ++i){
+            char ch = '0';
+            bool flag = true;
+            for(int j = 0; j < strs.size(); ++j){
+                if(ch == '0')
+                    ch = strs[j][i];
+                else if(strs[j][i] != ch){
+                    flag = false;
+                    break;
+                }
+            }
+            if(!flag) break;
+            ans.push_back(ch);
+        }
+        return ans;
+    }
+};
